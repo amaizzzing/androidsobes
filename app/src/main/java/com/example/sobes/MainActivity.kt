ecc43ae6171d956fb77a147d7177367c7e5310ff
amaizzzing <amaizzzing@gmail.com>
2021-11-13 01:34:35 +0500

    lesson1

--- app/src/main/java/com/example/sobes/MainActivity.kt
+++ app/src/main/java/com/example/sobes/MainActivity.kt
@@ -1,11 +1,75 @@
 package com.example.sobes
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.NavOptions
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.sobes.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private var activityMainBinding: ActivityMainBinding? = null
+    lateinit var navController: NavController
+
+    private val locationPermissionRequest =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            when {
+                permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) ||
+                permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
+                    navController.navigate(R.id.navigation_map)
+                } else -> {
+                    Toast.makeText(this, "ERROR PERMISSIONS", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding?.root)
+
+        initNavigation()
+
+        locationPermissionRequest.launch(
+            arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        )
+    }
+
+    private fun initNavigation() {
+        val navView: BottomNavigationView? = activityMainBinding?.bottomNavView
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
+        navController = navHostFragment.navController
+
+        activityMainBinding?.let { binding ->
+            navView?.setupWithNavController(navController)
+
+            binding.bottomNavView.setOnItemSelectedListener { menuItem ->
+                if (menuItem.itemId != navController.currentDestination?.id) {
+                    navController.navigate(
+                        menuItem.itemId,
+                        null,
+                        NavOptions.Builder().setPopUpTo(R.id.mobile_navigation, false).build()
+                    )
+                }
+                true
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        activityMainBinding = null
     }
 }
